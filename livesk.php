7c335ceecf842b33fb2077a40f8ad38de7f4f6f4
Bichel2 <118214039+Bichel2@users.noreply.github.com>
2024-09-04 18:40:16 +0300

    Update livesk.php

--- livesk.php
+++ livesk.php
@@ -1,25 +1,4 @@
-<?php
-      function sendStkPush() {
-        $token = getAccessToken();
-        $timestamp = date('YmdHis'); 
-        
-        $shortCode = " 4140577"; //sandbox -174379
-        $passkey = "3cf6e2fff13ba64211ff8e1e83f111630f0ad55450b70732404d629cd4d9a1bf";
-        
-        $stk_password = base64_encode($shortCode . $passkey . $timestamp);
-        
-       
-        
-        $url = "https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
-        
-        $headers = [
-          'Authorization: Bearer ' . $token,
-          'Content-Type: application/json'
-        ];
-        
-        $requestBody = array(
-          "BusinessShortCode" => $shortCode,
-          "Password" => $stk_password,
+
           "Timestamp" => $timestamp,
           "TransactionType" => "CustomerPayBillOnline", //till "CustomerBuyGoodsOnline"
           "Amount" => "10",
